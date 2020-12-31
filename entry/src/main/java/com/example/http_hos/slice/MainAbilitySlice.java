2c4bd43863066e3fb1ba944fbfa4179853197533
zhangerlong123 <352913998@qq.com>
2020-12-31 14:52:09 +0800

    123

--- entry/src/main/java/com/example/http_hos/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/example/http_hos/slice/MainAbilitySlice.java
@@ -9,22 +9,24 @@ import com.google.gson.JsonObject;
 import cz.msebera.android.httpclient.Header;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Text;
 import ohos.hiviewdfx.HiLog;
 import ohos.hiviewdfx.HiLogLabel;
 
 public class MainAbilitySlice extends AbilitySlice {
-    private HiLogLabel label=new HiLogLabel(HiLog.DEBUG,00*00101,"test-async-http");
+    private HiLogLabel label=new HiLogLabel(HiLog.ERROR,00*00101,"test-async-http");
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
+        Text tvResult=(Text) findComponentById(ResourceTable.Id_text);
         String url="http://apis.juhe.cn/simpleWeather/query";
         String key="32becf485f7f174d4385957b62f28f61";
         AsyncHttpClient client=new AsyncHttpClient();
         RequestParams params=new RequestParams();
         params.put("city","西安");
         params.put("key",key);
-        RequestHandle requestHandle = client.get("", new JsonHttpResponseHandler(){
+        RequestHandle requestHandle = client.get(url,params, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
                 super.onSuccess(statusCode, headers, responseString);
@@ -33,7 +35,7 @@ public class MainAbilitySlice extends AbilitySlice {
                     @Override
                     public void run() {
                         //更新UI
-//                        tvResult.setText(responseString);
+                        tvResult.setText(responseString);
                     }
                 });
             }
